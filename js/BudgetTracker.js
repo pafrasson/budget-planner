3bfb543eaa01493b4c5c61c73033dbb4334a64be
pafrasson <pafrasson@gmail.com>
2023-11-16 14:38:01 -0300

    feat: added firebase, implemented dinamic html

--- js/BudgetTracker.js
+++ js/BudgetTracker.js
@@ -13,61 +13,61 @@ export default class BudgetTracker {
     static html() {
         return `
         <table class="budget-tracker">
-        <thead>
-            <tr>
-                <th>Data</th>
-                <th>Descrição</th>
-                <th>Tipo</th>
-                <th>Quantidade</th>
-            </tr>
-        </thead>
-        <tbody class="entries">
-            <tr>
-                <td>
-                    <input class="input input-date" type="date">
-                </td>
-                <td>
-                    <input class="input input-description" type="text" placeholder="ex:(compras, conta de luz, etc.)">
-                </td>
-                <td>
-                    <select class="input input-tipo">
-                        <option value="income">Renda</option>
-                        <option value="expense">Gasto</option>   
-                    </select>
-                </td>
-                <td>
-                    <input class="input input-amount" type="number">
-                </td>
-                <td>
-                    <button type="button" class="delete-entry">&#10005;</button>
-                </td>
-            </tr>
-        </tbody>
-        <tbody>
-            <tr>
-                <td colspan="5" class="controls">
-                    <button type="button" class="new-entry">Adicionar</button>
-                </td>
-            </tr>
-        </tbody>
-        <tfoot>
-            <tr>
-                <td colspan="5" class="summary">
-                    <strong>Total:</strong>
-                    <span class="total">R$0.00</span>
-                </td>
-            </tr>
-        </tfoot>
-    </table>
+            <thead>
+                <tr>
+                    <th>Data</th>
+                    <th>Descrição</th>
+                    <th>Tipo</th>
+                    <th>Quantidade</th>
+                </tr>
+            </thead>
+            <tbody class="entries"></tbody>
+            <tbody>
+                <tr>
+                    <td colspan="5" class="controls">
+                        <button type="button" class="new-entry">Adicionar</button>
+                    </td>
+                </tr>
+            </tbody>
+            <tfoot>
+                <tr>
+                    <td colspan="5" class="summary">
+                        <strong>Total:</strong>
+                        <span class="total">R$0.00</span>
+                    </td>
+                </tr>
+            </tfoot>
+        </table>
         `;
     }
 
     static entryHtml() {
-
+        return `
+        <tr>
+            <td>
+                <input class="input input-date" type="date">
+            </td>
+            <td>
+                <input class="input input-description" type="text" placeholder="ex:(compras, conta de luz, etc.)">
+            </td>
+            <td>
+                <select class="input input-tipo">
+                    <option value="income">Renda</option>
+                    <option value="expense">Gasto</option>   
+                </select>
+            </td>
+            <td>
+                <input class="input input-amount" type="number">
+            </td>
+            <td>
+                <button type="button" class="delete-entry">&#10005;</button>
+            </td>
+        </tr>
+        `;
     }
 
     load() {
-
+        const entries = JSON.parse()
     }
 
     updateSummary() {
--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 import BudgetTracker from "./BudgetTracker.js";
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAHP6Xy9Y4Q71w77ba3b13Od2IssaFEG3o",
+    authDomain: "budget-planner-c8e63.firebaseapp.com",
+    projectId: "budget-planner-c8e63",
+    storageBucket: "budget-planner-c8e63.appspot.com",
+    messagingSenderId: "217432681396",
+    appId: "1:217432681396:web:e646ae18e0c73b0f0620e1"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
 
 new BudgetTracker("#app");
